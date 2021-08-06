ec4a5c84363f925643c75d5e15cf1e7e3839d725
HudaitRITAM <88477663+HudaitRITAM@users.noreply.github.com>
2021-08-06 18:07:19 +0530

    Add files via upload

---
+++ employee/employee.java
@@ -0,0 +1,13 @@
+package General;
+public class employee
+{
+    protected int empid;
+    private String ename;
+    public float earnings(int empid,String ename, int basic)
+    {
+        float t = 1.95f*basic;
+        System.out.println("\nEmployee ID = "+empid);
+        System.out.println("Employee Name = "+ename);
+        return t;
+    }
+}
