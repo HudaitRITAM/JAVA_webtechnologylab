8eda55f782ec7563fddb85c65d7d8f0f8ddc1e4b
HudaitRITAM <88477663+HudaitRITAM@users.noreply.github.com>
2021-08-09 08:45:40 +0530

    Add files via upload

---
+++ lab10_5/lab10_5.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class HrsException extends Exception {
+    public String getMessage() {
+        return "Invalid Hours!!";
+    }
+}
+class MinException extends Exception {
+    public String getString() {
+        return "Invalid minutes!!";
+    }
+}
+class SecException extends Exception {
+    public String toString() {
+        return "Invalid Seconds!!";
+    }
+}
+public class lab10_5 {
+    public static void Time(int hours, int mins, int secs) throws HrsException, MinException, SecException {
+        if (hours < 0 || hours > 24) {
+            throw new HrsException();
+        } else if (mins < 0 || mins > 60) {
+            throw new MinException();
+        } else if (secs < 0 || secs > 60) {
+            throw new SecException();
+        }
+        System.out.println("The valid time is:  " + hours + " hrs " + mins + " mins " + secs + " secs ");
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the hours, minute and seconds: ");
+        Scanner sc = new Scanner(System.in);
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+        int second = sc.nextInt();
+        try {
+            Time(hour, minute, second);
+        } catch (HrsException e) {
+            System.out.println(e.getMessage());
+        } catch (MinException e) {
+            System.out.println(e.getString());
+        } catch (SecException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
