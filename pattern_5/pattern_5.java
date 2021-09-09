994986a8f055539291dbc5b3b2b0b40a8e19a4af
HudaitRITAM <88477663+HudaitRITAM@users.noreply.github.com>
2021-09-09 12:22:35 +0530

    Add files via upload

---
+++ pattern_5/pattern_5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ 
+public class pattern_5
+{            
+    public static void main(String[] args)
+    {
+        int alphabet = 65;
+                for (int i = 0; i <= 3; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print((char) (alphabet + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
