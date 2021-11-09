84067ba9ab26b5cce98d78e9e423db2fd6639842
HudaitRITAM <88477663+HudaitRITAM@users.noreply.github.com>
2021-11-09 18:13:41 +0530

    Add files via upload

---
+++ swapvalue_wothird/swapvalue_wothird.java
@@ -0,0 +1,13 @@
+
+class swapvalue_wothird
+{
+    public static void main(String args[])
+    {
+        int x=30;
+        int y=45;
+        x=x+y;
+        y=x-y;
+        x=x-y;
+        System.out.println(" After swapping:" + " x =" + x + " ,y ="+ y);
+    }
+}
